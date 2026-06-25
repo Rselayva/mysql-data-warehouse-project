226c10cb942ed94ee86dfb0e04e74ffed3a47588
James <jameswu860817@gmail.com>
2026-06-25 18:08:19 +0800

    Modify 01_ddl_gold.sql for dim_products

--- scripts/gold/01_ddl_gold.sql
+++ scripts/gold/01_ddl_gold.sql
@@ -1,3 +1,6 @@
+-- =============================================================
+-- Create Dimension: datawarehouse_gold.dim_customers
+-- =============================================================
 CREATE OR REPLACE VIEW datawarehouse_gold.dim_customer AS
 SELECT
 	-- 1. Generate a unique Surrogate Key for the Gold Dimension Table
@@ -23,6 +26,44 @@ LEFT JOIN datawarehouse_silver.slv_erp_cust_az12 ca
 	ON ci.cst_key = ca.cid
 LEFT JOIN datawarehouse_silver.slv_erp_loc_a101 la
 	ON ci.cst_key = la.cid;
+
+-- =============================================================
+-- Create Dimension: datawarehouse_gold.dim_products
+-- =============================================================
+CREATE OR REPLACE VIEW datawarehouse_gold.dim_products AS
+SELECT
+	-- 1. Generate a Surrogate Key sorted by the internal unique Product ID
+	ROW_NUMBER() OVER (ORDER BY pi.prd_start_dt, pi.prd_key) AS product_key,
+    
+	pi.prd_id AS product_id,
+    pi.prd_key AS product_number,
+	pi.prd_nm AS product_name,
+	pi.cat_id AS category_id,
+    pc.cat AS category,
+	pc.subcat AS subcategory,
+    pc.maintenance AS maintenance,
+	pi.prd_cost AS cost,
+	pi.prd_line AS product_line,
+	pi.prd_start_dt AS start_date
+FROM datawarehouse_silver.slv_crm_prd_info pi
+LEFT JOIN datawarehouse_silver.slv_erp_px_cat_g1v2 pc
+ON pi.cat_id = pc.id
+-- 2. Filter out historical data to keep only the currently active products
+WHERE pi.prd_end_dt IS NULL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
